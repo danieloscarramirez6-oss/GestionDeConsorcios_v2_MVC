cc6c29d39c310b09e860641bc1f43fa60695153a
mpilarÃlegammari <mpilar.legammari@gmail.com>
2026-06-26 17:17:33 -0300

    Login draft

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+using GestionDeConsorcios_v2_MVC.Models;
+using GestionDeConsorcios_v2_MVC.Context;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GestionDeConsorcios_v2_MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly GestionDeConsorciosContext _context;
+
+        public AuthController(GestionDeConsorciosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new Usuario());
+        }
+
+        [HttpPost]
+        public IActionResult Login(Usuario usuario)
+        {
+            var user = _context.Usuarios.FirstOrDefault(u => u.Email == usuario.Email && u.PasswordHash == usuario.PasswordHash && u.Activo);
+            if (user != null)
+            {
+                if (user.Rol == RolUsuario.Administrador)
+                {
+                    // Redirigir a la vista de administrador (TO BE)
+                    return RedirectToAction("Administrador", "TOBE");
+                }
+                else if (user.Rol == RolUsuario.Propietario)
+                {
+                    // Redirigir a la vista de propietario (TO BE)
+                    return RedirectToAction("Propietario", "TOBE");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Usuario o contraseña incorrectos");
+            return View(usuario);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
