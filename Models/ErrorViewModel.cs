01301988e695c6fd8e933075ddbcc88d39b33123
mpilarÃlegammari <mpilar.legammari@gmail.com>
2026-06-26 15:15:25 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionDeConsorcios_v2_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
